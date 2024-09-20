17c64f4d7227c5a5a452d493b2b80325b0a80c0d
Sarah Schneider (RIT Student) <ses1431@rit.edu>
2024-09-20 14:45:43 -0400

    htmlResponse start

--- src/server.js
+++ src/server.js
@@ -3,33 +3,33 @@ const http = require('http');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-    console.log(request.url);
+  console.log(request.url);
 
-    switch (request.url) {
-        case "/success":
+  switch (request.url) {
+    case '/success':
 
-            break;
-        case "/badRequest":
+      break;
+    case '/badRequest':
 
-            break;
-        case "/unauthorized":
+      break;
+    case '/unauthorized':
 
-            break;
-        case "/forbidden":
+      break;
+    case '/forbidden':
 
-            break;
-        case "/internal":
+      break;
+    case '/internal':
 
-            break;
-        case "/notImplemented":
+      break;
+    case '/notImplemented':
 
-            break;
-        default:
+      break;
+    default:
 
-            break;
-    }
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on port 127.0.0.1:${port}`);
+  console.log(`Listening on port 127.0.0.1:${port}`);
 });
