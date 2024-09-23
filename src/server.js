5e687d563b545b7238df8fde0548c4b1e31fc47a
Sarah Schneider <143359626+ses011@users.noreply.github.com>
2024-09-23 01:47:45 -0400

    HTML API Assignment finished

--- src/server.js
+++ src/server.js
@@ -1,32 +1,36 @@
 const http = require('http');
+const responses = require('./responses.js');
+
+const urlStruct = {
+  '/': responses.getIndex,
+  '': responses.getIndex,
+  index: responses.getIndex,
+  '/success': responses.success,
+  '/badRequest': responses.badRequest,
+  '/unauthorized': responses.unauthorized,
+  '/forbidden': responses.forbidden,
+  '/internal': responses.internal,
+  '/notImplemented': responses.notImplemented,
+  '/notFound': responses.notFound,
+  '/style.css': responses.getStyle
+};
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
 
-  switch (request.url) {
-    case '/success':
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-      break;
-    case '/badRequest':
+  request.query = Object.fromEntries(parsedUrl.searchParams);
 
-      break;
-    case '/unauthorized':
+  request.acceptedTypes = request.headers.accept.split(',');
 
-      break;
-    case '/forbidden':
-
-      break;
-    case '/internal':
-
-      break;
-    case '/notImplemented':
-
-      break;
-    default:
-
-      break;
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  } else {
+    responses.notFound(request, response);
   }
 };
 
